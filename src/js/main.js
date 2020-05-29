71309a916a26cbc24ef259432052d63cc6c90c2d
Alex <budnikoleksii@gmail.com>
2020-05-29 05:22:08 +0300

    some tests, also added toggle animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,14 @@
 import './lib/lib';
 import $ from './lib/lib';
 
-$('button').on('click', function() {
-    $('div').eq(1).toggleClass('active');
+$('#first').on('click', () => {
+    $('div').eq(1).fadeToggle(800);
 });
 
-$('div').click(function() {
-    console.log($(this).index());
+$('[data-count="second"]').on('click', () => {
+    $('div').eq(2).fadeToggle(800);
 });
 
-// console.log($('div').eq(2).find('.some'));
-// fix it
-// console.log($('.some').closest('.findmeq').addClass('daggs'));
-
-// console.log($('.findme').siblings());
-// $('button').fadeIn(1800);
-
-// console.log($('button').html('hello'));
+$('button').eq(2).on('click', () => {
+    $('.w-500').fadeToggle(800);
+});
