5f67c2f5da359fc1692fb937c0a10134fe240ad9
Alex <budnikoleksii@gmail.com>
2020-05-30 12:29:26 +0300

    updated modals script to create modals dynamically

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,35 @@ $('button').eq(2).on('click', () => {
     $('.w-500').fadeToggle(800);
 });
 
+$('#trigger').click(() => $('#trigger').createModal({
+    text: {
+        title: 'Modal title',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum voluptas earum at nulla voluptatem laudantium ea ducimus aspernatur repellendus facilis repudiandae, itaque placeat nobis libero quas, eaque similique necessitatibus cum?'
+    },
+    btns: {
+        count: 3,
+        settings: [
+            [
+                'Close',
+                ['btn-danger', 'mr-10'],
+                true
+            ],
+            [
+                'Save changes',
+                ['btn-success'],
+                false,
+                () => {
+                    alert('Данные сохранены');
+                }
+            ],
+            [
+                'Another btn',
+                ['btn-warning', 'ml-10'],
+                false,
+                () => {
+                    alert('hello world');
+                }
+            ]
+        ]
+    }
+}));
